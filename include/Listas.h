39641a4d3ba1fb470c75fea470fa65cfb27c2f6b
malecal <malena.cal01@gmail.com>
2025-11-06 14:38:56 -0300

    Update Listas.h

--- include/Listas.h
+++ include/Listas.h
@@ -5,11 +5,51 @@
 #include <cstring>
 using namespace std;
 
-struct Playlist {
+class Playlist {
+private:
+    int idLista;
     char nombre[50];
-    char creador[50];
-};
+    int idSuscriptorCreador;
+    bool _estado;
+    int idPlaylist;
+    int idCancion;
 
+public:
+
+    Playlist(int idL = 0, const char* n = "", int idCreador = 0, bool est = true, int idP = 0, int idCan = 0) {
+        idLista = idL;
+        strcpy(nombre, n);
+        idSuscriptorCreador = idCreador;
+        _estado = est;
+        idPlaylist = idP;
+        idCancion = idCan;
+    }
+
+    void setIdLista(int id) { idLista = id; }
+    void setNombre(const char* n) { strcpy(nombre, n); }
+    void setIdSuscriptorCreador(int id) { idSuscriptorCreador = id; }
+    void setEstado(bool e) { _estado = e; }
+    void setIdPlaylist(int id) { idPlaylist = id; }
+    void setIdCancion(int id) { idCancion = id; }
+
+    int getIdLista() { return idLista; }
+    const char* getNombre() { return nombre; }
+    int getIdSuscriptorCreador() { return idSuscriptorCreador; }
+    bool getEstado() { return _estado; }
+    int getIdPlaylist() { return idPlaylist; }
+    int getIdCancion() { return idCancion; }
+
+
+    void mostrar() {
+        cout << "ID Lista: " << idLista
+             << " | Nombre: " << nombre
+             << " | ID Creador: " << idSuscriptorCreador
+             << " | Estado: " << (_estado ? "Activo" : "Inactivo")
+             << " | ID Playlist: " << idPlaylist
+             << " | ID Canción: " << idCancion
+             << endl;
+    }
+};
 
 class Listas {
 private:
