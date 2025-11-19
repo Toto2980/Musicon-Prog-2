84a78bb5d05b873566dfc682c7c884939c990a57
damidefe <112518200+damidefe@users.noreply.github.com>
2025-11-19 00:33:02 -0300

    Add files via upload

--- src/Fecha.cpp
+++ src/Fecha.cpp
@@ -1,15 +1,25 @@
 #include "Fecha.h"
+#include <string>
 
 Fecha::Fecha(){
     _dia = 0;
     _mes = 0;
     _anio = 0;
+
 }
 
-Fecha::Fecha(int dia, int mes, int anio){
+Fecha::Fecha(int minutos, int hora , int dia, int mes, int anio){
     _dia = dia;
     _mes = mes;
     _anio = anio;
+
+
+    _objetoHora.setHora(hora);
+    _objetoHora.setMinutos(minutos);
+ }
+
+HoraYMinutos Fecha::getHorario() {
+    return _objetoHora;
 }
 
 int Fecha::getDia(){
@@ -36,7 +46,11 @@ void Fecha::setAnio(int anio){
     _anio = anio;
 }
 
-std::string Fecha::toString(){
-    return std::to_string(_dia) + "/" + std::to_string(_mes) + "/" + std::to_string(_anio);
+void Fecha::setHorario(HoraYMinutos horario){
+    _objetoHora = horario;
 }
 
+std::string Fecha::toString(){
+
+    return std::to_string(_dia) + "/" + std::to_string(_mes) + "/" + std::to_string(_anio) + " " + _objetoHora.toString();
+}
---
+++ src/HoraYMinutos.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "HoraYMinutos.h"
+#include <string>
+
+using namespace std;
+
+HoraYMinutos::HoraYMinutos(){
+    _hora = 0;
+    _minutos = 0;
+}
+
+HoraYMinutos::HoraYMinutos(int hora, int minutos)
+{
+    _hora = hora;
+    _minutos = minutos;
+}
+
+
+int HoraYMinutos::getHora(){
+    return _hora;
+}
+
+
+int HoraYMinutos::getMinutos(){
+    return _minutos;
+}
+
+void HoraYMinutos::setHora(int hora) {
+    _hora = hora;
+}
+
+void HoraYMinutos::setMinutos(int minutos) {
+    _minutos = minutos;
+}
+
+std::string HoraYMinutos::toString() {
+
+    string minStr = std::to_string(_minutos);
+    if(_minutos < 10) minStr = "0" + minStr;
+
+    return std::to_string(_hora) + ":" + minStr;
+}
